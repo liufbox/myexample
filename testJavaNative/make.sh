7621c0e2c23291c9419107eb05baccaed1475f3b
liufbox <liufbox@163.com>
2021-08-12 14:12:28 +0800

    support linux

--- testJavaNative/make.sh
+++ testJavaNative/make.sh
@@ -4,5 +4,5 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
 javac HelloNative.java
 javah HelloNative
 gcc -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -fPIC -shared HelloNative.c -o libHelloNative.so
-java HelloNative
+java -cp ./ HelloNative
 
